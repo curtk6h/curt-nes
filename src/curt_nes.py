28ec9630ff8e1f28586cfbc0a38fb9ecfec9f17f
Curt Knobloch <>
2022-12-17 20:17:00 -0500

    Fix relative page crossing cost

--- src/curt_nes.py
+++ src/curt_nes.py
@@ -126,7 +126,7 @@ def play(mapper, registers=(0, 0, 0, 0, 0, 0), t=0):
         nonlocal t
         rel_addr = signed8(mem[pc+1])
         pc += 2
-        t += (((pc&0xFF)+rel_addr)>>7) & 2
+        t += (((pc&0xFF)+rel_addr)>>8) & 1
         return pc + rel_addr
     def _resolve_immediate(mem, pc):
         return pc + 1
@@ -345,22 +345,6 @@ def play(mapper, registers=(0, 0, 0, 0, 0, 0), t=0):
         return pc + 3
     
     # Branch Instructions
-    # Writes flags: none
-    # All branches are relative mode and have a length of two bytes. Syntax is "Bxx Displacement" or (better) "Bxx Label". See the notes on the Program Counter for more on displacements.
-    # 
-    # Branches are dependant on the status of the flag bits when the op code is encountered. A branch not taken requires two machine cycles. Add one if the branch is taken and add one more if the branch crosses a page boundary.
-    # 
-    # There is no BRA (BRanch Always) instruction but it can be easily emulated by branching on the basis of a known condition. One of the best flags to use for this purpose is the oVerflow which is unchanged by all but addition and subtraction operations.
-    # A page boundary crossing occurs when the branch destination is on a different page than the instruction AFTER the branch instruction. For example:
-    # 
-    # SEC
-    #   BCS LABEL
-    #   NOP
-    # A page boundary crossing occurs (i.e. the BCS takes 4 cycles) when (the address of) LABEL and the NOP are on different pages. This means that
-    #         CLV
-    #         BVC LABEL
-    #   LABEL NOP
-    # the BVC instruction will take 3 cycles no matter what address it is located at.
     # BPL (Branch on PLus)
     def _10_bpl(pc):
         nonlocal t
